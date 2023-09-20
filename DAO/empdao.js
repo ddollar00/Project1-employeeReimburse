953f44c4a495124c546c9f53679d76638905d3ec
Damion  Dollar <dollardamion@gmail.com>
2023-09-20 12:33:07 -0400

    foundation login feature

---
+++ DAO/empdao.js
@@ -0,0 +1,172 @@
+const AWS = require('aws-sdk');
+
+// In order to perform AWS operations using the aws-sdk library,
+// we need to actually "log in" to AWS through an IAM user
+// This would require you to create an IAM user with the appropriate permissions
+// for using DynamoDB, and you would need to generate an access key to use to log into that user
+// from here
+
+// As previously mentioned a few days ago, aws-sdk will automatically look
+// for the access key and secret access key from the following 2 environment variables
+// 1. AWS_ACCESS_KEY_ID=<access key value>
+// 2. AWS_SECRET_ACCESS_KEY=<secret access key>
+// It will use the values of those two environment variables to log in as the IAM user
+
+// You should also set the AWS_DEFAULT_REGION environment variable to the AWS region you are using
+
+AWS.config.update({
+    region: 'us-east-2'
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+
+function postLogin(username, password) {
+    const params = {
+        TableName: 'user',
+        IndexName: 'username-index', // Specify the secondary index name
+        KeyConditionExpression: 'username = :username',
+        ExpressionAttributeValues: {
+            ':username': username,
+        },
+    };
+
+    try {
+        // Try to query the user based on the username
+        return docClient.query(params).promise()
+            .then(data => {
+                const user = data.Items[0]; // Assuming usernames are unique
+
+                // Check if the user exists and if the provided password matches
+                if (user && user.password === password) {
+                    return { authenticated: true, user };
+                } else {
+                    return { authenticated: false };
+                }
+            })
+            .catch(err => {
+                console.error('Error retrieving user:', err);
+                throw err; // Rethrow the error for handling elsewhere if needed
+            });
+    } catch (err) {
+        console.error('Error:', err);
+        throw err; // Rethrow the error for handling elsewhere if needed
+    }
+}
+
+
+
+function getUnResolvedTickets(status) {
+    const params = {
+        TableName: 'tickets',
+        FilterExpression: '#c = :value',
+        ExpressionAttributeNames: {
+            '#c': 'status'
+        },
+        ExpressionAttributeValues: {
+            ':value': status
+        }
+    };
+
+    return docClient.scan(params).promise();
+}
+function getPreviousTickets(name) {
+    const params = {
+        TableName: 'tickets',
+        IndexName: 'name-index',
+        KeyConditionExpression: '#name = :value',
+        ExpressionAttributeNames: {
+            '#name': 'name',
+        },
+        ExpressionAttributeValues: {
+            ':value': name,
+        }
+    };
+
+    return docClient.query(params).promise();
+}
+
+function postRegister(user_id, username, password) {
+
+    const params = {
+        TableName: 'user',
+        Item: {
+            user_id,
+            admin: false,
+            username,
+            password
+        }
+    };
+    const paramss = {
+        TableName: 'user',
+        IndexName: 'username-index',
+        KeyConditionExpression: 'username = :username',
+        ExpressionAttributeValues: {
+            ':username': username,
+        },
+    };
+    return docClient.query(paramss).promise()
+        .then(data => {
+            const user = data.Items[0];
+
+
+            if (user) {
+                return [0, { register: false }];
+            } else {
+                return [docClient.put(params).promise(), { register: true }];
+            }
+        })
+        .catch(err => {
+            console.error('Error retrieving user:', err);
+            throw err; // Rethrow the error for handling elsewhere if needed
+        });
+
+};
+function postSubTicket(ticket_id, amount, description, name, type) {
+
+    const params = {
+        TableName: 'tickets',
+        Item: {
+            ticket_id,
+            amount,
+            description,
+            status: 'pending',
+            type,
+            name
+        }
+    }
+
+    return docClient.put(params).promise();
+};
+function putUpdateTicketStatus(ticket_id, newStatus) {
+    const params = {
+        TableName: 'tickets',
+        Key: {
+            ticket_id
+        },
+        UpdateExpression: 'set #n = :value,#name = :valuee',
+        ExpressionAttributeNames: {
+            '#n': 'status',
+            '#name': 'name'
+        },
+        ExpressionAttributeValues: {
+            ':value': newStatus,
+            ':valuee': 'default'
+        }
+    }
+
+
+    return docClient.update(params).promise();
+}
+
+
+
+module.exports = {
+    putUpdateTicketStatus,     //done
+    postSubTicket,            //done
+    postRegister,            //done
+    getUnResolvedTickets,
+    getPreviousTickets,   //done
+    postLogin             //done
+
+};
---
+++ app.js
@@ -0,0 +1,178 @@
+// HTTP Methods
+
+const PORT = 3000;
+const express = require('express');
+const server = express();
+const dao = require('./DAO/empdao.js');
+const bodyParser = require('body-parser');
+const uuid = require('uuid')
+
+
+server.use(bodyParser.json());
+function validateNewUser(req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        req.body.valid = false;
+        next();
+    } else {
+        req.body.valid = true;
+        next();
+    }
+}
+function validateNewTicket(req, res, next) {
+    if (!req.body.description || !req.body.name || !req.body.amount || !req.body.type) {
+        req.body.valid = false;
+        next();
+    } else {
+        req.body.valid = true;
+        next();
+    }
+}
+
+server.post('/login', (req, res) => {
+    const body = req.body;
+
+    dao.postLogin(body.username, body.password)
+        .then((data) => {
+            if (data.authenticated) {
+                res.send({
+                    message: "Login Successful"
+                });
+            } else {
+                res.send({
+                    message: "Login Unsuccessful"
+                });
+            }
+
+        })
+        .catch((err) => {
+            res.send({
+                message: "Incorrect username or password"
+            });
+            console.error(err);
+        })
+
+
+
+});
+
+
+server.post('/register', validateNewUser, (req, res) => {
+    const body = req.body;
+    if (req.body.valid) {
+        dao.postRegister(uuid.v4(), body.username, body.password)
+            .then((data) => {
+                if (data[1].register == false) {
+                    res.send({
+                        message: "Username taken"
+                    });
+                } else {
+                    res.send({
+                        message: "Successfully registered User!"
+                    });
+                }
+
+            })
+            .catch((err) => {
+
+                console.error(err);
+            })
+    } else {
+        res.send({
+            message: "Invalid Item properties"
+        })
+    }
+});
+server.post('/submitTicket', validateNewTicket, (req, res) => {
+    const body = req.body;
+    if (req.body.valid) {
+        dao.postSubTicket(uuid.v4(), body.amount, body.description, body.name, body.type)
+            .then((data) => {
+
+                res.send({
+                    message: "Ticket submitted successfully"
+                });
+
+
+            })
+            .catch((err) => {
+                res.send({
+                    message: "Ticket not submitted"
+                });
+                console.error(err);
+            })
+    } else {
+        res.send('missing ticket information')
+    }
+
+})
+server.put('/update', (req, res) => {
+    const body = req.body;
+
+    dao.putItem(body.grocery_item_id, body.newName)
+        .then((data) => {
+            res.send({
+                message: "Successfully updated Item!"
+            })
+        })
+        .catch((err) => {
+            res.send({
+                message: "Failed to update Item!"
+            });
+            console.error(err);
+        })
+
+});
+server.get('/tickets', (req, res) => {
+    const status = req.query.status;
+    dao.getUnResolvedTickets(status)
+        .then((data) => {
+            res.send(data.Items);
+        })
+        .catch((err) => {
+            res.send({
+                message: 'no pending tickets'
+            });
+        })
+});
+server.get('/tickets/old', (req, res) => {
+    const name = req.query.name;
+
+    dao.getPreviousTickets(name)
+        .then((data) => {
+            res.send(data.Items);
+        })
+        .catch((err) => {
+            res.send({
+                message: 'no pending tickets'
+            });
+        })
+});
+server.put('/tickets/:id', (req, res) => {
+    const ticket_id = req.params.id;
+    const status = req.query.status;
+    const adminStat = req.headers.currentemployee;
+    console.log(req.headers);
+    if (adminStat == 1) {
+
+
+        dao.putUpdateTicketStatus(ticket_id, status)
+            .then((data) => {
+                res.send(`status was updated to ${status}`);
+            })
+            .catch((error) => {
+                res.send('ticket not found')
+            })
+    } else {
+        res.send(
+            "This is forbidden for employee's"
+        );
+    }
+});
+server.listen(PORT, () => {
+    console.log(`server is listening on port: ${PORT}`);
+});
+
+
+
+
+module.exports = server;
